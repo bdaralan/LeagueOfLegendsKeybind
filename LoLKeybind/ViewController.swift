a71edb0e5b6be56b3eb10a042bed1cc2abf600ed
iDara09 <bd.daralan@yahoo.com>
2018-11-24 09:55:01 -0800

    Initial Commit

---
+++ LoLKeybind/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  LoLKeybind
+//
+//  Created by Dara Beng on 11/24/18.
+//  Copyright © 2018 Dara Beng. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
